373aeda405e8fb10a0368d46c62ed23cb622767c
Sherwin688 <sherwin.dcosta688@gmail.com>
2023-05-29 23:06:30 +0530

    added dashboard api

--- index.js
+++ index.js
@@ -62,12 +62,22 @@ app.post("/find",async(req,res)=>{
 }
     }
 )
-app.delete("/delete/:id",async(req,res)=>{
+app.delete("/delete",async(req,res)=>{
     try {
-        const datetask = await DateTasks.findByIdAndDelete(req.params.id)
-        res.send("Deleted Successfully")
+        console.log(req.body.id)
+        const datetask = await DateTasks.findOne({"date":req.body.date})
+        const newDateTasks = datetask.tasks.filter((task) => task.id !== req.body.id);
+        await newDateTasks.save()
+        // console.log(newDateTasks)
+        res.send({message:"success",data:newDateTasks})
+
+    
+
+        // const newDateTasks = datetask.filter((task)=>task.id!==req.body.id)
+
     } catch (error) {
         console.log(error);
+        res.send("error")
     }
 })
 app.post("/add",async(req,res)=>{
@@ -98,13 +108,150 @@ app.post("/addDailyTask",async(req,res)=>{
     }
 })
 
-// app.get("/test",async(req,res)=>{
+app.post('/getLineChart', async (req, res) => {
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+      var months2 = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+      var monthNames = {
+  "January": 1,
+  "February": 2,
+  "March": 3,
+  "April": 4,
+  "May": 5,
+  "June": 6,
+  "July": 7,
+  "August": 8,
+  "September": 9,
+  "October": 10,
+  "November": 11,
+  "December": 12
+};
+
+
+    try {
+      const results = await DateTasks.aggregate([
+        {
+          $project: {
+            month: { $month: { $dateFromString: { dateString: { $concat: [{ $substr: ['$date', 6, 4] }, '-', { $substr: ['$date', 3, 2] }, '-', { $substr: ['$date', 0, 2] }] } } } },
+            tasks: '$tasks.status'
+          }
+        },
+        {
+          $unwind: '$tasks'
+        },
+        {
+          $group: {
+            _id: { month: '$month', status: '$tasks' },
+            count: { $sum: 1 }
+          }
+        },
+        {
+          $group: {
+            _id: '$_id.month',
+            counts: {
+              $push: {
+                k: '$_id.status',
+                v: '$count'
+              }
+            }
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            month: '$_id',
+            counts: { $arrayToObject: '$counts' }
+          }
+        },
+        {
+          $sort: {
+            month: 1
+          }
+        }
+      ]
+      
+      
+      ).exec().then(async(results)=>{
+        var data = [];
+        // console.log(results[0].month)
+          
+                
+            results.map((res)=>{
+                data.push({
+                    "month":res.month,
+                    "complete":res.counts.complete===undefined?0:res.counts.complete,
+                    "incomplete":res.counts.incomplete,
+                })
+                months2.indexOf(months[res.month-1]);
+                if ( months2.indexOf(months[res.month-1]) !== -1) {
+                    months2.splice( months2.indexOf(months[res.month-1]), 1);
+                  }
+            })   
+
+            months2.map((month)=>{
+                data.push({
+                    "month":monthNames[month],
+                    "complete":0,
+                    "incomplete":0
+                })
+            })
+            data.sort((a, b) => a.month - b.month);
+            var dataset =[];
+            var incompleteTasks=[]
+            
+            data.map((val)=>{
+                dataset.push(val.complete)
+                incompleteTasks.push(val.incomplete)
+            })
+            console.log(req.params.date);
+            const sum = dataset.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+            const incompleteTasksSum = incompleteTasks.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+            const datetask = await DateTasks.findOne({"date":req.body.date})
+            var todayComplete = 0
+            var todayInComplete = 0
+            datetask.tasks.map((task)=>{
+                if(task.status==="complete"){
+                    todayComplete++
+                }
+                else{
+                    todayInComplete++
+
+                }
+            })
+            res.json({"dataset":dataset,"totalTasks":sum,"incompleteTasks":incompleteTasksSum,"totalTasks":incompleteTasksSum+sum,"pieChart":[todayComplete,todayInComplete]});
+      });
+  
    
-//     const dailytasks = await DailyTasks.findOne().sort({ field: 'asc', _id: -1 }).limit(1)
-
-//     res.json(dailytasks.tasks)
-
-// })
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  });
 app.put("/update",async(req,res)=>{ 
     const datetask = await DateTask.findOne({"date":req.body.date})
         datetask.date=req.body.date
@@ -117,6 +264,10 @@ app.put("/update",async(req,res)=>{
     }
 })
 
+app.get("/dashboarddata",async(req,res)=>{
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth() + 1;
+})
 app.listen(8000,()=>{
     console.log("Server Running")
 })
